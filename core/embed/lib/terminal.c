e8720fb8135c331ac789fe5cf98d2ed116b87449
obrusvit <vit.obrusnik@gmail.com>
2024-04-22 14:51:14 +0200

    WIP - attempt to hotfix build on apple
    DO NOT MERGE

--- core/embed/lib/terminal.c
+++ core/embed/lib/terminal.c
@@ -53,7 +53,7 @@ static uint64_t term_glyph_bits(char ch) {
     uint8_t bytes[8];
   } result = {0};
 
-  if (ch > ' ' && ch < 128) {
+  /* if (ch > ' ' && ch < 128) { */
     const uint8_t *b = &Font_Bitmap[(ch - ' ') * 5];
 
     for (int y = 0; y < 7; y++) {
@@ -62,7 +62,7 @@ static uint64_t term_glyph_bits(char ch) {
                          ((b[2] & mask) ? 32 : 0) + ((b[3] & mask) ? 16 : 0) +
                          ((b[4] & mask) ? 8 : 0);
     }
-  }
+  /* } */
   return result.u64;
 }
 
